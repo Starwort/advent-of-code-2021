7a923bb6b54059ccc043a14c68bb06fed363a3d4
starwort <tcphone93@gmail.com>
2021-12-01 05:13:22 +0000

    first commit

---
+++ day_01.py
@@ -0,0 +1,42 @@
+import aoc_helper
+
+raw = aoc_helper.fetch(1, 2021)
+
+
+def parse_raw():
+    ...
+
+
+data = aoc_helper.extract_ints(raw)
+
+
+def part_one():
+    first, *rest = data
+    increased = 0
+
+    for i in rest:
+        if i > first:
+            increased += 1
+        first = i
+    return increased
+
+
+def part_two():
+    first, second, third, *rest = data
+    last = first + second + third
+    increased = 0
+
+    for i in rest:
+        first = second
+        second = third
+        third = i
+        new = first + second + third
+        if new > last:
+            increased += 1
+        last = new
+
+    return increased
+
+
+aoc_helper.lazy_submit(day=1, year=2021, solution=part_one)
+aoc_helper.lazy_submit(day=1, year=2021, solution=part_two)
---
+++ day_18.py
@@ -0,0 +1,22 @@
+import aoc_helper
+
+raw = aoc_helper.fetch(18, 2021)
+
+
+def parse_raw():
+    ...
+
+
+data = parse_raw()
+
+
+def part_one():
+    ...
+
+
+def part_two():
+    ...
+
+
+aoc_helper.lazy_submit(day=18, year=2021, solution=part_one)
+aoc_helper.lazy_submit(day=18, year=2021, solution=part_two)
