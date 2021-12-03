c397e57e5d8e9cbc78bbebbe08c293933aeb481e
starwort <tcphone93@gmail.com>
2021-12-03 04:19:56 +0000

    Add day 2

--- day_02.py
+++ day_02.py
@@ -5,18 +5,31 @@ raw = aoc_helper.fetch(2, 2021)
 
 
 def parse_raw():
-    ...
-
-
-data = parse_raw()
+    for line in raw.splitlines():
+        direction, distance = line.split()
+        if direction == "up":
+            yield 0, -int(distance)
+        elif direction == "down":
+            yield 0, int(distance)
+        elif direction == "forward":
+            yield int(distance), 0
 
 
 def part_one():
-    ...
+    sum_x, sum_y = 0, 0
+    for x, y in parse_raw():
+        sum_x += x
+        sum_y += y
+    return sum_x * sum_y
 
 
 def part_two():
-    ...
+    sum_x, sum_y, sum_aim = 0, 0, 0
+    for x, aim in parse_raw():
+        sum_aim += aim
+        sum_y += x * sum_aim
+        sum_x += x
+    return sum_x * sum_y
 
 
 aoc_helper.lazy_submit(day=2, year=2021, solution=part_one)
