59fe68dc75058815ca2102ed7f2d6a857b5f1a5a
starwort <tcphone93@gmail.com>
2021-12-07 05:08:45 +0000

    1805 / 611 :( tried to be smart for p1 but it didn't work and just costed me time

--- day_07.py
+++ day_07.py
@@ -5,18 +5,28 @@ raw = aoc_helper.fetch(7, 2021)
 
 
 def parse_raw():
-    ...
+    return extract_ints(raw)
 
 
 data = parse_raw()
 
 
 def part_one():
-    ...
+    return min(
+        data.mapped(lambda x: abs(x - target)).sum()
+        for target in range(data.min(), data.max() + 1)
+    )
+
+
+def tri(x):
+    return x * (x + 1) // 2
 
 
 def part_two():
-    ...
+    return min(
+        data.mapped(lambda x: tri(abs(x - target))).sum()
+        for target in range(data.min(), data.max() + 1)
+    )
 
 
 aoc_helper.lazy_submit(day=7, year=2021, solution=part_one)
