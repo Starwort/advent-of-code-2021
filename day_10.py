521b7edb2d9240d83d92c0f2c737eb8cc7be5569
starwort <tcphone93@gmail.com>
2021-12-10 05:10:10 +0000

    75/71, could have been better if I'd actually read the question and took the median not the sum the first time

--- day_10.py
+++ day_10.py
@@ -5,18 +5,77 @@ raw = aoc_helper.fetch(10, 2021)
 
 
 def parse_raw():
-    ...
+    return list(raw.splitlines())
 
 
 data = parse_raw()
 
 
+def parse_corrupt(line):
+    stack = []
+    for c in line:
+        if c == "(":
+            stack.append(c)
+        elif c == ")":
+            if stack.pop() != "(":
+                return c
+        elif c == "[":
+            stack.append(c)
+        elif c == "]":
+            if stack.pop() != "[":
+                return c
+        elif c == "{":
+            stack.append(c)
+        elif c == "}":
+            if stack.pop() != "{":
+                return c
+        elif c == "<":
+            stack.append(c)
+        elif c == ">":
+            if stack.pop() != "<":
+                return c
+    return ""
+
+
 def part_one():
-    ...
+    return (
+        data.mapped(parse_corrupt)
+        .filtered(None)
+        .mapped(lambda i: {")": 3, "]": 57, "}": 1197, ">": 25137}[i])
+        .sum()
+    )
+
+
+def autocomplete(line):
+    stack = []
+    for c in line:
+        if c == "(":
+            stack.append(c)
+        elif c == ")":
+            stack.pop()
+        elif c == "[":
+            stack.append(c)
+        elif c == "]":
+            stack.pop()
+        elif c == "{":
+            stack.append(c)
+        elif c == "}":
+            stack.pop()
+        elif c == "<":
+            stack.append(c)
+        elif c == ">":
+            stack.pop()
+    stack = stack[::-1]
+    score = 0
+    for c in stack:
+        score *= 5
+        score += " ([{<".index(c)
+    return score
 
 
 def part_two():
-    ...
+    new_data = data.filtered(lambda i: parse_corrupt(i) == "")
+    return new_data.mapped(autocomplete).median()
 
 
 aoc_helper.lazy_submit(day=10, year=2021, solution=part_one)
