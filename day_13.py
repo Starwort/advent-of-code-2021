88d8cb633cfb5f4c11a7367a77158b9e2c913e6f
starwort <tcphone93@gmail.com>
2021-12-13 05:11:45 +0000

    261/150

--- day_13.py
+++ day_13.py
@@ -2,21 +2,77 @@ import aoc_helper
 from aoc_helper import extract_ints, frange, irange, iter, list, map, range, tail_call
 
 raw = aoc_helper.fetch(13, 2021)
+# raw = """6,10
+# 0,14
+# 9,10
+# 0,3
+# 10,4
+# 4,11
+# 6,0
+# 6,12
+# 4,1
+# 0,13
+# 10,12
+# 3,4
+# 3,0
+# 8,4
+# 1,10
+# 2,14
+# 8,10
+# 9,0
+
+# fold along y=7
+# fold along x=5"""
 
 
 def parse_raw():
-    ...
+    points, instructions = raw.split("\n\n")
+    parsed_points = set(extract_ints(points).chunked(2))
+    parsed_instructions = list()
+    for instruction in instructions.splitlines():
+        instruction = instruction.removeprefix("fold along ")
+        if instruction[0] == "x":
+            parsed_instructions.append((int(instruction[2:]), 0))
+        else:
+            parsed_instructions.append((0, int(instruction[2:])))
+    return parsed_points, parsed_instructions
 
 
-data = parse_raw()
+points, instructions = parse_raw()
+
+
+def fold(points, instruction):
+    x, y = instruction
+    new_points = set()
+    if x == 0:
+        for px, py in points:
+            if py > y:
+                new_points.add((px, 2 * y - py))
+            else:
+                new_points.add((px, py))
+    else:
+        for px, py in points:
+            if px > x:
+                new_points.add((2 * x - px, py))
+            else:
+                new_points.add((px, py))
+    return new_points
 
 
 def part_one():
-    ...
+    return len(fold(points, instructions[0]))
 
 
 def part_two():
-    ...
+    my_points = points
+    for instruction in instructions:
+        my_points = fold(my_points, instruction)
+    max_x = max(point[0] for point in my_points)
+    max_y = max(point[1] for point in my_points)
+    for y in irange(0, max_y):
+        for x in irange(0, max_x):
+            print("#" if (x, y) in my_points else " ", end="")
+        print()
 
 
 aoc_helper.lazy_submit(day=13, year=2021, solution=part_one)
