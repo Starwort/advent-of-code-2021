fa47e027fa0f79636a5dc50edd823162de9ee386
starwort <tcphone93@gmail.com>
2021-12-15 05:54:20 +0000

    Use aoc_helper.djikstras, return to list concatenation

--- day_15.py
+++ day_15.py
@@ -2,48 +2,20 @@ from collections import defaultdict
 from heapq import heappop, heappush, heappushpop
 
 import aoc_helper
-from aoc_helper import (
-    decode_text,
-    extract_ints,
-    frange,
-    irange,
-    iter,
-    list,
-    map,
-    range,
-    tail_call,
-)
+from aoc_helper import djikstras, list, map, range
 
 raw = aoc_helper.fetch(15, 2021)
 
 
 def parse_raw():
-    return list(list(map(int, line)) for line in raw.splitlines())
+    return list(map(int, line).collect() for line in raw.splitlines())
 
 
 data = parse_raw()
 
 
 def part_one():
-    to_visit = []
-    heappush(to_visit, (0, (0, 0)))
-    visited = set()
-    target = data[0].len() - 1, data.len() - 1
-    while True:
-        risk, (x, y) = heappop(to_visit)
-        if (x, y) in visited:
-            continue
-        if (x, y) == target:
-            return risk
-        visited.add((x, y))
-        if x > 0:
-            heappush(to_visit, (risk + data[y][x - 1], (x - 1, y)))
-        if x < target[0]:
-            heappush(to_visit, (risk + data[y][x + 1], (x + 1, y)))
-        if y > 0:
-            heappush(to_visit, (risk + data[y - 1][x], (x, y - 1)))
-        if y < target[1]:
-            heappush(to_visit, (risk + data[y + 1][x], (x, y + 1)))
+    return djikstras(data)
 
 
 def part_two():
@@ -55,30 +27,14 @@ def part_two():
         + row.mapped(lambda x: (x + 3) % 9 + 1)
         for row in data
     )
-    new_data = list(
-        row.mapped(lambda x: (x + increment - 1) % 9 + 1)
-        for increment in range(5)
-        for row in new_data
+    new_data = (
+        new_data
+        + new_data.mapped(lambda row: row.mapped(lambda x: x % 9 + 1))
+        + new_data.mapped(lambda row: row.mapped(lambda x: (x + 1) % 9 + 1))
+        + new_data.mapped(lambda row: row.mapped(lambda x: (x + 2) % 9 + 1))
+        + new_data.mapped(lambda row: row.mapped(lambda x: (x + 3) % 9 + 1))
     )
-    to_visit = []
-    heappush(to_visit, (0, (0, 0)))
-    visited = set()
-    target = new_data[0].len() - 1, new_data.len() - 1
-    while True:
-        risk, (x, y) = heappop(to_visit)
-        if (x, y) in visited:
-            continue
-        if (x, y) == target:
-            return risk
-        visited.add((x, y))
-        if x > 0:
-            heappush(to_visit, (risk + new_data[y][x - 1], (x - 1, y)))
-        if x < target[0]:
-            heappush(to_visit, (risk + new_data[y][x + 1], (x + 1, y)))
-        if y > 0:
-            heappush(to_visit, (risk + new_data[y - 1][x], (x, y - 1)))
-        if y < target[1]:
-            heappush(to_visit, (risk + new_data[y + 1][x], (x, y + 1)))
+    return djikstras(new_data)
 
 
 aoc_helper.lazy_submit(day=15, year=2021, solution=part_one)
